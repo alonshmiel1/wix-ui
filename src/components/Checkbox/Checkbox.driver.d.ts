3fc5c459eb9de0a5d90b42d7cd2f6d43de55c5e3
builduser <builduser@wix.com>
2018-02-04 15:34:38 +0000

    gh-pages

---
+++ src/components/Checkbox/Checkbox.driver.d.ts
@@ -0,0 +1,27 @@
+export declare const checkboxDriverFactory: ({ element, eventTrigger }: {
+    element: any;
+    eventTrigger: any;
+}) => {
+    element: () => any;
+    exists: () => boolean;
+    click: () => any;
+    mouseEnter: () => any;
+    mouseLeave: () => any;
+    focus: () => any;
+    getType: () => any;
+    isChecked: () => boolean;
+    getTextContent: () => any;
+    children: () => any;
+    tickmark: () => any;
+    indeterminateMark: () => any;
+    isIndeterminate: () => boolean;
+    input: () => any;
+    isDisabled: () => boolean;
+    styles: {
+        getMinWidth: () => string;
+        getWidth: () => string;
+        getHeight: () => string;
+        getPadding: () => string;
+        getBorderRadius: () => string;
+    };
+};
